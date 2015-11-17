141f67ab72e98a508c35ee0a216f3a61307b674e
Josh Fletcher <jfletcher@nuxeo.com>
2015-11-16 16:03:32 -0800

    Fix import paths.

--- Gruntfile.js
+++ Gruntfile.js
@@ -71,7 +71,8 @@ module.exports = function (grunt) {
               require('connect-livereload')({
                 port: LIVERELOAD_PORT
               }),
-              mountFolder(yeomanConfig.app)
+              mountFolder(yeomanConfig.app),
+              mountFolder('bower_components')
             ];
           }
         }
